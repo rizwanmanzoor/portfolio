4abcf10651dd3ac270f0de1bf86818ce80781be2
rizwanmanzoor <rizwanmanzoor200@gmail.com>
2026-01-22 02:33:05 +0500

    uninstall react-helmet

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,9 @@
 import { Hero } from "@/sections/contact";
-import { Helmet } from "react-helmet-async";
 
 export default function Contact() {
   return (
-    <>
-      <Helmet>
-        <title>Contact Web Developer in Lahore | Rizwan Manzoor</title>
-        <meta
-          name="description"
-          content="Contact Rizwan Manzoor, a professional web developer in Lahore, Pakistan, for React, UI/UX, WordPress, and WooCommerce development services."
-        />
-        <link
-          rel="canonical"
-          href="https://rizwanmanzoor.netlify.app/contact"
-        />
-      </Helmet>
-      
-      <div className="bg-[#0f0f0f] min-h-screen">
-        <Hero />
-      </div>
-    </>
+    <div className="bg-[#0f0f0f] min-h-screen">
+      <Hero />
+    </div>
   );
 }
